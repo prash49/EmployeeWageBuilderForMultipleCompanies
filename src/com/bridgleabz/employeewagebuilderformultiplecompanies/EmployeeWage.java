0ae20a78c5430fbf1da5579c93cdc1fd31c93708
Prashanth <imprash4u@gmail.com>
2021-09-02 19:39:59 +0530

    Added:Employee Attendance Check

--- src/com/bridgleabz/employeewagebuilderformultiplecompanies/EmployeeWage.java
+++ src/com/bridgleabz/employeewagebuilderformultiplecompanies/EmployeeWage.java
@@ -3,5 +3,13 @@ package com.bridgleabz.employeewagebuilderformultiplecompanies;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println(".....Welcome to Employee Wage Builder Program For Multiple companies...");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("employee is present");
+        } else {
+            System.out.println("employee is absent");
+        }
     }
 }
+
